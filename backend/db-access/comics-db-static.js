3258072ee4a38d288db9084ab8e5e1715e01d3b6
LuisMejia007 <paranoidandroidz180@gmail.com>
2021-11-06 15:03:29 -0700

    Connected data from express app to mobile client.

--- backend/db-access/comics-db-static.js
+++ backend/db-access/comics-db-static.js
@@ -7,6 +7,30 @@ const comics = [
     comic_publication_date: "June 1938",
     comic_cover_img: "dhbwbed",
   },
+  {
+    comic_id: 1308,
+    comic_name: "Actions Comics #2",
+    comic_writer: "Jerry Siegel",
+    comic_artist: "Joe Shuster",
+    comic_publication_date: "June 1938",
+    comic_cover_img: "dhbwbed",
+  },
+  {
+    comic_id: 1309,
+    comic_name: "Actions Comics #3",
+    comic_writer: "Jerry Siegel",
+    comic_artist: "Joe Shuster",
+    comic_publication_date: "June 1938",
+    comic_cover_img: "dhbwbed",
+  },
+  {
+    comic_id: 1310,
+    comic_name: "Actions Comics #4",
+    comic_writer: "Jerry Siegel",
+    comic_artist: "Joe Shuster",
+    comic_publication_date: "June 1938",
+    comic_cover_img: "dhbwbed",
+  },
 ];
 
 module.exports = comics;
--- comiclysm-app/screens/TabTwoScreen.tsx
+++ comiclysm-app/screens/TabTwoScreen.tsx
@@ -1,15 +1,62 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, VirtualizedList } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const ComicCard = ({ title }) => (
+  <View>
+    <Text> { title } </Text>
+  </View>
+);
+
 export default function TabTwoScreen() {
+
+  const [comicList, setComicList] = React.useState([]);
+  
+  const getComic = (data: any, index: number) => {
+    return comicList[index];
+  }
+
+  const getComicCount = () =>{ return comicList.length }
+
+  
+  const getComics = async() => {
+    const response = await fetch("http://192.168.254.26:3000/api/comics");
+
+    await response.json()
+    .then((any) =>{ 
+      console.log("Debug :" , any);
+      setComicList(any);
+    })
+    .catch((e) => {
+      console.log("Error", e);
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+
+      <VirtualizedList
+        data = { comicList }
+        initialNumToRender = { 0 }
+        renderItem = { 
+          ({item}) => 
+          <ComicCard title = { item.comic_name } />
+        }
+        keyExtractor = { comic => comic.comic_id }
+        getItem = { getComic }
+        getItemCount = { getComicCount }
+      >
+
+      </VirtualizedList>
+      <Button 
+        title="Get Comics"
+        color="#f194ff"
+        onPress = { () => { getComics() }} 
+      />
     </View>
   );
 }
